ccf55ef72f94b6969c75f6b8feeb3047304ac2d5
Blaze380 <144695514+Blaze380@users.noreply.github.com>
2023-09-22 19:35:41 +0300

    table override fixed!
    I fixed the bug "adding a 3x3 table under the previous" and improving the code adding comments

--- scripts/index.js
+++ scripts/index.js
@@ -17,29 +17,28 @@ class Player{
  var player = [];
  for (let i = 0;i<2; i++){
      player[i] = new Player("Joãozinh123");
- }
-var container = document.getElementById("container");
-var screen;
-//var screen = document.getElementsByClassName("array_table");
-container.addEventListener("click", function (event) {
-    /**
-     * This condition was made to avoid to cause an exeption because of the table array 
-     * doesn't have an ID!
-     */
+}
+const container = document.getElementById("container");
+const screen = document.getElementsByClassName("array_table");
+screen[0].addEventListener("click", function (event) {
+    //This condition was made to avoid to cause an exeption because of the table array doesn't have an ID!
     if(event.target.id !==""){
         addDice(event.target.id);
     }
 });
-var positions = [];
-var isOcuppied = [];
-var rows = 2; var columns = 2;
-var table = [];
-var quit = false;
-var currentPlayerDice = "X";
+var playerBar = null;
+var playerDetails = [];
+var positions = []; //This matrix is for the game table
+var isOcuppied = []; //This matrix is do declare if the positions are ocuppied or not.
+var rows = 2; var columns = 2; ///I think that those are useless LOL
+const table = []; //This array is to make the display flex and organize the table(I'll remove cause useless)
+var quit = false; //Variable used for the while loop
+var currentPlayerDice = "X"; //This variable is to store the current player dice to verify the winner
+
 /**
  * This Loop Creates a basic matrix  3x3 for gaming
  * "#Don't ask me why you have to suffer to declare  a matrix beacuse idk LOL."
- */
+*/
 for (let i = 0; i <= rows; i++){
     positions[i] = [];
     isOcuppied[i] = [];
@@ -48,16 +47,19 @@ for (let i = 0; i <= rows; i++){
         isOcuppied[i][j] = false;
     }
 }
+createTable();
+
+//This method removes the table and put a newer
 function resetTable() {
     for (let i = 0; i < positions.length; i++){
-    for (let j = 0; j < positions.length; j++){
-        table[i] = null;
-        positions[i][j] = null;
-        isOcuppied[i][j] = false;
-    }    
+        screen[0].removeChild(table[i]);
+        isOcuppied[i].fill(false);
+    }
+    createTable();
 }
-}
-createTable();
+
+
+
 /**
  * This method gets the position of the div receveing the ID(event.target.id) of the div.
  * And verifies if the current div is ocuppied with a dice. If isn't ocuppied, will add a dice.
@@ -73,16 +75,20 @@ function addDice(arrayData) {
         won = verifyWinner();
         if (won) {
             alert("I won!");
-            createTable();
+            resetTable();
         } else {
             verifyDraw();
         }
         changePlayer();
     } else {
-        //#REMOVE THIS!!!!!!
-        console.log("Error!");
+        console.log("This position is already ocuppied!!");
     }
 }
+
+/**
+ * This method verifies if all the positions has been put a dice and
+ * declare draw!
+ */
 function verifyDraw() {
     let cont = 0;
     for (let i = 0; i < isOcuppied.length; i++){
@@ -97,6 +103,8 @@ function verifyDraw() {
     }
 }
 
+
+//This method changes the player dice
 function changePlayer() {
     switch (currentPlayerDice) {
         case "X":
@@ -106,6 +114,11 @@ function changePlayer() {
             currentPlayerDice = "X";
     }
 }
+
+/**
+ * 
+ * @returns 
+ */
 function verifyWinner() {
     let counter = 0;
     let hasWinner = false;
@@ -194,7 +207,6 @@ function firstPhase() {
         column++;
     }
     if ((cont !== positions.length)) {
-      /**REMOVE THIS!!! */  console.log("1a fase");
         cont = 0;
         row = 0;
             for (let j = 0; j < positions[0].length; j++){
@@ -214,25 +226,38 @@ function firstPhase() {
     }
     return cont;
 }
+
 //This function Creates a div's.
 function createTable() {
-    //resetTable();
-    screen = document.createElement("div");
-    screen.className = "array_table";
-
+    // playerBar = document.createElement("div");
+    // playerBar.className = "player_bar";
+    // for (let i = 0; i < 3; i++){
+    //     playerDetails[i] = document.createElement("div");
+    //     playerDetails[i].id = "player" + (i + 1);
+    //     switch (i) {
+    //         case 0:
+    //             playerDetails[i].innerText = "X";
+    //             break;
+    //         case 1:
+    //             playerDetails[i].innerText = "O";
+    //             break;
+    //     }
+    //     playerBar.appendChild(playerDetails[i]);
+    // }
+    // container.appendChild(playerBar);
 
     /**
      * This div represents a number of rows in the table.
      * and receives a class name "table" to receive the estilization in the CSS.
-    */
-   for (let i = 0; i < positions.length; i++){
-       table[i] = document.createElement("div");
-       table[i].className = "table";
+     */
+    for (let i = 0; i < positions.length; i++){
+        table[i] = document.createElement("div");
+        table[i].className = "table";
 
 
-       /**
-        * This div represents the spaces that the dices will gonna be placed
-        * and receive the class name "array" to receive the estilization in the CSS.
+        /**
+         * This div represents the spaces that the dices will gonna be placed
+         * and receive the class name "array" to receive the estilization in the CSS.
          */
         for (let j = 0; j < positions[0].length; j++){
             positions[i][j] = document.createElement("div");
@@ -240,7 +265,7 @@ function createTable() {
             positions[i][j].id = i + "/" + j; //The id of the current div will receive it's position on the array
             table[i].appendChild(positions[i][j]);//The "array" div's will be appended in the  "table" div's 
         }
-        screen.appendChild(table[i]);//Therefore the "table" div's will be appended in the "container" div to be shown in the screen.
+        screen[0].appendChild(table[i]);//Therefore the "table" div's will be appended in the "container" div to be shown in the screen.
     }
-    container.appendChild(screen);
+
 }
